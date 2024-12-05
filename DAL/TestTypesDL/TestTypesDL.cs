6858945a1b71fe14aa81b46ebfd202cdef2ca990
Tareq Abu sharkh <149328700+Tareq-Bilal@users.noreply.github.com>
2024-12-05 18:29:37 +0300

    Add files via upload

---
+++ DAL/TestTypesDL/TestTypesDL.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TestTypesDataAccessLayer
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=TAREQ\\DB1;Database=DVLD;Integrated Security=true;";
+    }
+
+    public static class clsTestTypesDataAccess
+    {
+        public static bool GetTestTypeInfoByID(int TestTypeID, ref string TestTypeTitle, ref string TestTypeDescription, ref decimal TestTypeFees)
+        {
+            bool isFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT * FROM TestTypes WHERE TestTypeID = @TestTypeID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+            try
+            {
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    isFound = true;
+
+                    TestTypeID = (int)reader["TestTypeID"];
+                    TestTypeTitle = (string)reader["TestTypeTitle"];
+                    TestTypeDescription = (string)reader["TestTypeDescription"];
+                    TestTypeFees = (decimal)reader["TestTypeFees"];
+
+                }
+                else
+                {
+                    isFound = false;
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex) { }
+            finally { connection.Close(); }
+
+            return isFound;
+
+        }
+
+        public static decimal GetTestTypeFeeByID(int TestTypeID)
+        {
+            decimal TestTypeFees = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "select TestTypeFees from TestTypes where TestTypeID = @TestTypeID";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+            try
+            {
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+
+                    TestTypeFees = (decimal)reader["TestTypeFees"];
+
+                }
+                else
+                {
+                    TestTypeFees = -1;
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex) { }
+            finally { connection.Close(); }
+
+            return TestTypeFees;
+
+        }
+
+        public static int AddNewTestType(string TestTypeTitle, string TestTypeDescription, decimal TestTypeFees)
+        {
+
+            int ID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"INSERT INTO TestTypes VALUES (@TestTypeTitle, @TestTypeDescription, @TestTypeFees)
+        SELECT SCOPE_IDENTITY()";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            command.Parameters.AddWithValue("@TestTypeTitle", TestTypeTitle);
+
+            command.Parameters.AddWithValue("@TestTypeDescription", TestTypeDescription);
+
+            command.Parameters.AddWithValue("@TestTypeFees", TestTypeFees);
+
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    ID = insertedID;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                //Console.WriteLine(Error:  + ex.Message);
+
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+
+
+            return ID;
+
+        }
+        public static bool UpdateTestType(int TestTypeID, string TestTypeTitle, string TestTypeDescription, decimal TestTypeFees)
+        {
+            int rowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"UPDATE TestTypes
+	SET	TestTypeTitle = @TestTypeTitle,
+	TestTypeDescription = @TestTypeDescription,
+	TestTypeFees = @TestTypeFees	WHERE TestTypeID = @TestTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+
+            command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+            command.Parameters.AddWithValue("@TestTypeTitle", TestTypeTitle);
+
+            command.Parameters.AddWithValue("@TestTypeDescription", TestTypeDescription);
+
+            command.Parameters.AddWithValue("@TestTypeFees", TestTypeFees);
+
+
+            try { connection.Open(); rowsAffected = command.ExecuteNonQuery(); }
+            catch (Exception ex) { }
+            finally { connection.Close(); }
+
+            return (rowsAffected > 0);
+
+        }
+        public static bool DeleteTestType(int TestTypesID)
+        {
+            int rowsAffected = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "DELETE TestTypes WHERE TestTypeID = @TestTypeID";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestTypesID", TestTypesID);
+
+            try
+            {
+                connection.Open();
+                rowsAffected = command.ExecuteNonQuery();
+            }
+            catch (Exception ex) { }
+            finally { connection.Close(); }
+
+
+            return (rowsAffected > 0);
+
+        }
+
+        public static bool IsTestTypeExist(int TestTypeID)
+        {
+            bool isFound = false;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT Found=1 FROM TestTypes WHERE TestTypeID= @TestTypeID";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@TestTypeID", TestTypeID);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                isFound = reader.HasRows;
+                reader.Close();
+            }
+            catch (Exception ex) { }
+            finally { connection.Close(); }
+
+
+            return isFound;
+
+        }
+
+        public static DataTable GetAllTestTypes()
+        {
+
+            DataTable dt = new DataTable();
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "SELECT * FROM TestTypes";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows) dt.Load(reader);
+                reader.Close();
+            }
+            catch (Exception ex) { }
+            finally { connection.Close(); }
+
+
+            return dt;
+        }
+
+        public static int GetNumberOfTestTypes()
+        {
+            int NumberOfTestTypes = 0;
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+            string query = "select count (*) NumberOfTestTypes from TestTypes";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    NumberOfTestTypes = (int)reader["NumberOfTestTypes"];
+                }
+                else
+                {
+                    return 0;
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex) { }
+            finally { connection.Close(); }
+
+            return NumberOfTestTypes;
+
+
+        }
+
+
+    }
+
+}
